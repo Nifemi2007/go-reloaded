243bfb79343afc89bc991324376fb8b119e961d6
noyinloy <oyinyinkanifemi@gmail.com>
2026-03-09 14:07:15 +0100

    added more functions

--- main.go
+++ main.go
@@ -1,32 +1,78 @@
 package main
-import ("fmt"
+import (
+	"fmt"
 "strings"
 "strconv"
 // "slices"
-// "os"
+"os"
 )
 
+func capitalize(str string) string{
+	capSlice := []byte(str)
+	capSlice[0] = capSlice[0] - 32
+	str = string(capSlice)
+	// fmt.Println(str)
+	return str
+}
 
+func uppercase(str string) string{
+	str = strings.ToUpper(str)
+	return str
+}
+
+func lowercase(str string) string{
+	str = strings.ToLower(str)
+	return str
+}
+
+func formatBasedOnNumb(key, numb string, sent []string, index int) {
+	// replace ',' in the keyword with ')'
+	modKey := strings.Replace(key, ",", ")",1)
+
+	// convert string to number and replace ')' with empty space
+	modNumb,_ := strconv.Atoi(strings.Replace(numb, ")", "", 1))
+
+	if modKey == "(cap)" {
+		for i:=0; i<modNumb; i++{
+		   sent[index - 1]	= capitalize(sent[index - 1 ])
+			index = index - 1
+		}
+	} else if modKey == "(low)" {
+		for i:=0; i<modNumb; i++{
+		   sent[index - 1]	= lowercase(sent[index - 1 ])
+			index = index - 1
+		}
+	} else if modKey == "(up)" {
+		for i:=0; i<modNumb; i++ {
+			sent[index - 1] = uppercase(sent[index - 1])
+			index = index - 1
+		}
+	}
+
+}
 
 func formatSentence(s string) string{
 	words := strings.Split(s, " ")
+	// var output string
+	formatedWords := []string{}
 
 	for idx, _ := range words {
 
 		// Capitalize
 		if words[idx] == "(cap)" {
-			capSlice := []byte(words[idx -1])
-			capSlice[0] = capSlice[0] - 32
-			words[idx - 1] = string(capSlice)
+			words[idx-1] = capitalize(words[idx-1])
 			words[idx] = ""
+			// words = append(words[:idx], words[idx + 1:])
+			
+
 			// To Uppercase
 		} else if words[idx] == "(up)" {
-			words[idx - 1] = strings.ToUpper(words[idx - 1])
+			words[idx-1] = uppercase(words[idx-1])
 			words[idx] = ""
 
 			// To LowerCase
 		} else if words[idx] == "(low)" {
-			words[idx - 1] = strings.ToLower(words[idx -1])
+			words[idx - 1] = lowercase(words[idx-1])
 			words[idx] = ""
 		} else if words[idx] == "(hex)" {
 			// convert from base 16 to base 10
@@ -42,92 +88,98 @@ func formatSentence(s string) string{
 
 			// convert int to string
 			val := strconv.FormatInt(s, 10)
-			words[idx -1] = val
+			words[idx - 1] = val
 			
 			words[idx] = ""
 
 			// To execute keywords based on number
 		} else if strings.Contains(words[idx],"(") {
+			// fmt.Println(words[idx])
 			slice := words[idx:idx+2]
-			fmt.Println(slice[0])
-		}
+			formatBasedOnNumb(slice[0], slice[1], words, idx)
+			words[idx] = ""
+			words[idx + 1] = ""
+
+			// Changing A to An
+		}  else if words[idx] == "A" || words[idx] == "a" {
+			slice := []byte(words[idx + 1])
+			if slice[0] == 'a' || slice[0] == 'e' || slice[0] == 'i' || slice[0] == 'o' || slice[0] == 'u' || slice[0] == 'h' {
+				words[idx] = words[idx] + "n"
+			}
+
+			// To remove space infront of punctuation
+		}  else if (strings.Contains(words[idx],"." ) || strings.Contains(words[idx],"," ) || strings.Contains(words[idx],"!" ) || strings.Contains(words[idx],"?" ) || strings.Contains(words[idx],":" ) || strings.Contains(words[idx],";" ) ){
+			slice := []byte(words[idx])
+			
+			// fmt.Println(string(slice))
+
+			if (len(slice) > 1) && (slice[0] < 48 && slice[1] < 48) {
+				words[idx - 1] = words[idx - 1] + words[idx]
+				
+				// To print the remaining alphabet if the following words is not a symbol
+				if slice[1] > 64 {
+					words[idx] = string(slice[1:])
+				} else {
+					words[idx] = ""
+				}
+				
+			} else if slice[0] < 48 || (slice[0] > 57 && slice[0] < 65){
+				if words[idx - 1] == "" {
+					words[idx - 2] = words[idx - 2] + string(slice[0])
+				} else {
+					words[idx - 1] = words[idx - 1] + string(slice[0])
+				}
+					words[idx] = string(slice[1:])
+			}
+			
+		
+	 	} 
+	
+	
 	}
-	// output := strings.Join(words, " ")
+
+	
+	// To remove spaces
+	for index,_:= range words {
+		if words[index] == "" {
+			// fmt.Println(words[index - 1])
+			continue
+		}   else {
+			formatedWords = append(formatedWords, words[index])
+		} 
+	}
+
+	
+	output := strings.Replace(strings.Join(formatedWords," "), " ,", ",", -1)
+	// output:= strings.Join(formatedWords," ")
 	// fmt.Println(output)
-	return s
+	return output
 }
 
 func main() {
 	// // Getting the input from .txt
-	// inputPath := os.Args[1]
-	// content, err := os.ReadFile(inputPath)
-	// if err != nil {
-	// 	fmt.Printf("File Error: %v", err)
-	// }	
+	inputPath := os.Args[1]
+	content, err := os.ReadFile(inputPath)
+	if err != nil {
+		fmt.Printf("File Error: %v", err)
+	}	
 
-	defectSent := "it (cap) was the best of times, it was the worst of times (up) , it was the age of wisdom, it was the age of foolishness (cap, 6) , it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of darkness, it was the spring of hope, IT WAS THE (low, 3) winter of despair."
-	formatSentence(defectSent)
+	// defectSent := "it (cap) was the best of times, it was the worst of times (up) , it was the age of wisdom, it was the age of foolishness (cap, 6) , it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of darkness, it was the spring of hope, IT WAS THE (low, 3) winter of despair."
+	// formatSentence(defectSent)
 
-	defectSent2 := "Simply add 42 (hex) and 10 (bin) and you will see the result is 68."
-	formatSentence(defectSent2)
+	// defectSent2 := "Simply add 42 (hex) and 10 (bin) and you will see the result is 68."
+	// formatSentence(defectSent2)
 
+	// formatSentence("Punctuation tests are ... kinda boring ,what do you think ?")
+	// formatSentence("There it was. A amazing rock!")
+	// formatSentence("There is no greater agony than bearing a untold story inside you.")
 
-	// // Saving the Output
-	// outputPath := os.Args[2]
-	// data := []byte(formatSentence(string(content)))
-	// err2 := os.WriteFile(outputPath, data, 0644)
-	// if err2 != nil {
-	// 	fmt.Printf("Error: %v", err2)
-	// }
+	// Saving the Output
+	outputPath := os.Args[2]
+	data := []byte(formatSentence(string(content)))
+	err2 := os.WriteFile(outputPath, data, 0644)
+	if err2 != nil {
+		fmt.Printf("Error: %v", err2)
+	}
 	
 }
-// // func returnDecimal() {
-
-// // }
-
-// func getKeyword(words string) {
-// 	// keywords := []string{}
-// 	// if words[i] = '('  get the word until you see ')'
-// 	var a,b int;
-// 	keywordSlice := []string{}
-// 	sliceIndex := []int{}
-// 	defectWord := []string{}
-// 	for i, val := range words {
-// 		// if(string(val) == " ") {
-// 		// 	fmt.Println(val)
-// 		// }
-
-// 		// Get the keyword
-// 		if (string(val) == "(") {
-// 			a = i
-
-// 			defectWord = append(defectWord, words[:(a-1)])
-// 		} else if(string(val) == ")") {
-// 			b = i
-// 			keywordSlice = append(keywordSlice, words[(a+1):b])
-// 				// break;
-// 			}  
-
-		
-// 	}
-
-// 	// Edit word based on keyword
-	
-	
-// 	// splitKeyword := strings.Split(keywordSlice, ",")
-// 	fmt.Println(keywordSlice[2])
-// 	fmt.Println(sliceIndex)
-// 	// fmt.Println(defectWord)
-// 	// fmt.Println(splitKeyword)
-// }
-
-// func main() {
-// 	// result := getKeyword("hey")
-// 	testSentence := "it (cap) was the best of times, it was the worst of times (up) , it was the age of wisdom, it was the age of foolishness (cap, 6) , it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of darkness, it was the spring of hope, IT WAS THE (low, 3) winter of despair."
-// 	getKeyword(testSentence)
-
-// 	fmt.Println(strings.ToUpper("heyIU"))
-// 	// arr1 := [2]string{"man", "Bola"}
-// 	// fmt.Println(arr1.split(","))
-// 	// fmt.Println("hey")
-// }
